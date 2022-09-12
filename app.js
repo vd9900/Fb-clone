0f397b38451bc9916df3bcfc12035ce83e8a7fbd
vinith <96461411+vd9900@users.noreply.github.com>
2022-09-12 21:36:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,3 @@ function twoSum(nums, target) {
  for(let i=0;i<nums.length;i++){
      for(var j=i+1;j<nums.length;j++){
       console.log(i,j)
-      if(nums[i]+nums[j]===target){
-             anwarray.push(
