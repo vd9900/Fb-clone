565f59accc22cfe3794c154dab98c19b0062a088
vinith <96461411+vd9900@users.noreply.github.com>
2022-11-09 22:01:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // // popover
 // const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 // const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 
